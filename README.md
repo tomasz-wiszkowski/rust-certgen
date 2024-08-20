f86d190c39ff667d82d2d9883f9c4c57c477abe6
Tomasz Wiszkowski <tomasz.wiszkowski@gmail.com>
2024-08-19 21:38:59 -0700

    Ugly but works
    Change-Id: I93a1d2978c779851a695cb78542d571449d5fae1

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# rust-certgen
-Self-signed certificates for your local network made simple
+# Certificate generator
+
+_Self-signed certificates for your local network made simple._
+
+All keys and generated certificates are owned by you. You are the only person responsible for keeping these secrets secure.
+
+## Basic usage
+
+1. Edit `certgen.toml` file.
+  * Apply your desired defaults. Be mindful of setting the certificate validity - too long and it will be rejected by most browsers! 2 years seems safe.
+  * Add your desired server names (and/or IP addresses).
+2. Run the tool (e.g. `cargo run`)
+3. Preserve key files and the `root_ca.crt` file. These files will be used to identify fingerprint of your servers and generate updated certificates
+4. Deploy certificates to your devices
+5. Install the `root_ca.crt` on your systems:
+  * on OSX: 
+    * Open Keychain > System > Certificates. 
+    * Add certificate.
+    * Double click on newly added certificate. Its name will match the name you configured in the `certgen.toml` file
+    * Expand `Trust` and then `Allow all`
+  * on Windows:
+    * Right-click the `root_ca.crt` file
+    * Select _Install Certificate_
+    * Select either _Current User_ or _Local Machine_,
+    * Select _Place in the following store_ > _Trusted Root Certification Authorities_
+    * Continue to import
+  * on Linux
+    * Open the web browser you use daily
+    * Open Settings > Security
+    * Look for _Manage Certificates_
+    * Import the `root_ca.crt` file, and make sure to trust it.
+  * on Android
+    * Download the `root_ca.crt` file
+    * Open Settings, search for _CA Certificate_
+    * Click _Install Anyway_
+    * Select your certificate
+
+## FAQs
+
+### Why is everything warning me against installation?
+
+This is because you are technically installing a certificate that has the power of "blessing" domains as "secure". Normally this is controlled by a set of _truly trusted_ set of Root Certifcate Authorities (CAs). Your self-generated CA is now joining the club, and it is granted the same capability as all the other, pre-selected Root CAs. If your `root_ca.key` leaks **your security will be compromised**, and the owner may generate certificates and impersonate any website they want. Because of this you have to keep all the keys in secure store.
+
+
+## TODOs
+
+* Clean up the code,
+* Add password to guard the `root_ca.key`.
--- src/main.rs
+++ src/main.rs
@@ -70,13 +70,73 @@ fn read_key<T: AsRef<str>>(path: T) -> Result<PKey<Private>> {
     Ok(PKey::from_rsa(ca_key)?)
 }
 
+fn read_root_crt(net: &Network) -> Result<X509> {
+    if !std::fs::try_exists(&net.root_ca_crt)? {
+        if !ask(format!(
+            "Certificate {} does not exist. Create new one?",
+            net.root_ca_crt
+        )) {
+            bail!("Aborted by user");
+        }
+
+        let key = read_key(&net.root_ca_key)?;
+
+        let mut x509_builder = X509::builder()?;
+        x509_builder.set_version(2)?;
+        x509_builder.set_pubkey(&key)?;
+
+        let mut name_builder = openssl::x509::X509NameBuilder::new()?;
+        name_builder.append_entry_by_nid(Nid::COMMONNAME, &net.name)?;
+        name_builder.append_entry_by_nid(Nid::ORGANIZATIONNAME, &net.name)?;
+        name_builder.append_entry_by_nid(Nid::ORGANIZATIONALUNITNAME, &net.name)?;
+        name_builder.append_entry_by_nid(Nid::PKCS9_EMAILADDRESS, &net.email)?;
+
+        if let Some(country) = net.country.as_ref() {
+            name_builder.append_entry_by_nid(Nid::COUNTRYNAME, country)?;
+        }
+        if let Some(province) = net.province.as_ref() {
+            name_builder.append_entry_by_nid(Nid::STATEORPROVINCENAME, province)?;
+        }
+
+        let name = name_builder.build();
+        x509_builder.set_subject_name(&name)?;
+        // Self-signed.
+        x509_builder.set_issuer_name(&name)?;
+
+        x509_builder.append_extension(
+            openssl::x509::extension::ExtendedKeyUsage::new()
+                .server_auth()
+                .build()?,
+        )?;
+
+        x509_builder.append_extension(
+            openssl::x509::extension::BasicConstraints::new()
+                .ca()
+                .build()?,
+        )?;
+
+        // Set validity period to 100 years.
+        let not_before = openssl::asn1::Asn1Time::days_from_now(0)?;
+        let not_after = openssl::asn1::Asn1Time::days_from_now(365 * 100)?;
+        x509_builder.set_not_before(&not_before)?;
+        x509_builder.set_not_after(&not_after)?;
+
+        // Self-sign the certificate
+        x509_builder.sign(&key, MessageDigest::sha256())?;
+
+        let x509 = x509_builder.build();
+        std::fs::write(&net.root_ca_crt, x509.to_pem()?)?;
+    }
+
+    Ok(X509::from_pem(&std::fs::read(&net.root_ca_crt)?)?)
+}
+
 fn main() -> Result<()> {
     let toml_str = std::fs::read_to_string("certgen.toml")?;
     let config: Config = toml::from_str(&toml_str)?;
 
     let ca_pkey = read_key(&config.network.root_ca_key)?;
-
-    let ca_cert = X509::from_pem(&std::fs::read(&config.network.root_ca_crt)?)?;
+    let ca_cert = read_root_crt(&config.network)?;
     let ca_subject_name = ca_cert.subject_name();
 
     for (site_name, site_cfg) in &config.site_certs {
